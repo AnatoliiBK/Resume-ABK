b0d4b7b31732a1309a01665c35ab33ead35a5b36
AnetRo <beatrice.ccq@gmail.com>
2023-07-23 00:39:58 +0300

    des

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,26 @@
-import LIKAVITOS from "./LIKAVITOS.jpg"
+import plus from "./plus.jpg";
 
 export const App = () => {
   return (
     <div className="doc">
       
       <aside className="sidebar">
-        <img src={LIKAVITOS} alt="Anatolii Bazan - Koval" style={{width: "320px", height: "230px"}} />
+        <img src={plus} alt="Anatolii Bazan - Koval" className="img" style={{width: "320px", height: "379px"}} />
+        <h1 className="is-visible">ANATOLII BAZAN - KOVAL</h1>
+        <p className="is-visible">FULL STACK DEVELOPER</p>
         <h2>CONTACTS</h2>
           <ul className="list-zero">
-            <li className="list-item">
-              <a href="tel:+380681480342" className="contact-link">+38 068 148 03 42</a>
+            <li className="list-contacts list-item">
+              <a href="tel:+380681480342" className="contact-link">Phone</a>
             </li>
-            <li className="list-item">
-              <a href="mailto: beatrice.ccq@gmail.com">Email</a>
+            <li className="list-contacts list-item">
+              <a href="mailto: beatrice.ccq@gmail.com" className="contact-link">Email</a>
             </li>
-            <li className="list-item">
-              <a href="https://github.com/AnatoliiBK" >GitHub</a>
+            <li className="list-contacts list-item">
+              <a href="https://github.com/AnatoliiBK"className="contact-link">GitHub</a>
             </li>
-            <li className="list-item">
-              <a href="https://www.linkedin.com/in/anatolii-bazan-koval-b61b00274/">LinkedIn</a>
+            <li className="list-contacts list-item">
+              <a href="https://www.linkedin.com/in/anatolii-bazan-koval-b61b00274/"className="contact-link">LinkedIn</a>
             </li>
           </ul>
         
@@ -30,15 +32,18 @@ export const App = () => {
 
         <h2>TECH SKILLS</h2>
         <ul className="list">
-            <li className="list-item">(Перелік технічних навичок)</li>
-            <li className="list-item">(що ви володієте)</li>
-            <li className="list-item">(у вашій сфері діяльності)</li>
+            <li className="list-item">HTML, CSS, SCSS</li>
+            <li className="list-item">JavaScript</li>
+            <li className="list-item">React</li>
+            <li className="list-item">Redux</li>
         </ul>
 
         <h2>SOFT SKILLS</h2>
         <ul className="list">
-            <li className="list-item">(Перелік навичок м'яких навичок)</li>
-            <li className="list-item">(комунікаційні, лідерські, адаптаційні тощо)</li>
+            <li className="list-item">Ability to listen</li>
+            <li className="list-item">Attention to detail</li>
+            <li className="list-item">Patience</li>
+            <li className="list-item">Compliance</li>
         </ul>
 
         
@@ -53,10 +58,15 @@ export const App = () => {
 
     
     <div className="main-content">
-      <h1>ANATOLII BAZAN - KOVAL</h1>
-        <p>FRONT-END DEVELOPER</p>
+      <h1 className="is-hidden">ANATOLII BAZAN - KOVAL</h1>
+        <p className="is-hidden">FULL STACK DEVELOPER</p>
         <h2>SUMMARY</h2>
-        <p>(Тут ви можете коротко описати свій досвід, навички і цілі в кар'єрному зростанні)</p>
+        <p>Technique is my hobby. In order to get an understanding of the principles of mechanical
+           engineering, he studied at the university. Even then, I liked the design, which was 
+           performed with the help of drawing tools. This also comes in handy for equipment repair work, 
+           which I do now. What comes from your own efforts and is useful to someone else makes you needed. 
+           Modernity has other ways of designing and implementing your own and someone's plans. Therefore, 
+           the profession of Full Stack Developer is another way to realize yourself, including for someone's success.</p>
         <h2>WORK EXPERIENCE</h2>
         <ul className="list">
           <li className="list-item">
@@ -71,13 +81,21 @@ export const App = () => {
         <h2>PROJECT EXPERIENCE</h2>
         <ul className="list">
           <li className="list-item">
-          <h3>(Назва проекту 1)</h3>
-        <p>(Деталі проекту та ваш внесок)</p>
+          
+            <a href="https://ritulina.github.io/parcel-project-template/" className="project-link">
+              <h3>mimiно (Restaurant and hotel complex)</h3>
+            </a>
+            <a href="https://github.com/Ritulina/parcel-project-template" className="project-link">GitHub</a>
+              <p>Site with active content to get acquainted with the amenities of the complex and to book places in the hotel and restaurant.<br />Role: Development of the section "Contacts".</p>
           </li>
           <li className="list-item">
-          <h3>(Назва проекту 2)</h3>
-        <p>(Деталі проекту та ваш внесок)</p>
-        {/* Додавайте додаткові проекти, якщо необхідно */}
+            <a href="https://badyanch.github.io/BOOKSHELF/" className="project-link">
+              <h3>Bookshelf</h3>
+            </a>
+            <a href="https://github.com/badyanch/BOOKSHELF" className="project-link">GitHub</a>
+          
+              <p>A site with modern functionality and an intuitive interface for searching books and ordering from several resources.<br />Role: Development of a theme change.</p>
+        
           </li>
         </ul>
         
@@ -86,11 +104,17 @@ export const App = () => {
         <h2>EDUCATION</h2>
         <ul className="list">
           <li className="list-item">
-            <h3>IT School GoIT</h3>
+            <a href="https://goit.global/ua/" className="education-link">
+              <h3>IT School GoIT</h3>
+            </a>
+            
             <p>Fullstack Developer | 2022 – 2023</p>
           </li>
           <li className="list-item">
-            <h3>Ternopil Instrument Engineering Institute</h3>
+            <a href="https://tntu.edu.ua/?p=uk/main" className="education-link">
+              <h3>Ternopil Instrument Engineering Institute</h3>
+            </a>
+            
             <p>Engineer | 1988 - 1993</p>
           </li>
         </ul>
